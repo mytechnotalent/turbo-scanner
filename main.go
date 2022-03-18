f5b8af30e946962b5c4815395e75e3545a5d7fcd
Kevin Thomas <kevin@mytechnotalent.com>
2022-03-18 13:25:15 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,9 @@
-// turbo-scanner v0.1.0
+// Terms Of Use
+// ------------
+// Do NOT use this on any computer you do not own or are not allowed to run this on.
+// You may NEVER attempt to sell this, it is free and open source.
+// The authors and publishers assume no responsibility.
+// For educational purposes only.
 
 package main
 
