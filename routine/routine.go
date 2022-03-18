ca0fffed5cd75c406ee42a4a2298aad9d29712cc
Kevin Thomas <kevin@mytechnotalent.com>
2022-03-18 13:24:00 -0400

    Update routine.go

--- routine/routine.go
+++ routine/routine.go
@@ -1,3 +1,10 @@
+// Terms Of Use
+// ------------
+// Do NOT use this on any computer you do not own or are not allowed to run this on.
+// You may NEVER attempt to sell this, it is free and open source.
+// The authors and publishers assume no responsibility.
+// For educational purposes only.
+
 // Package routine provides port scanning functionality.
 package routine
 
